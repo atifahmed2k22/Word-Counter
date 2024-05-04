a77a50b0d83bce05a92b1751db6fcb6d139f94dc
atif ahmed <atifahmedpak2k22@gmail.com>
2024-05-05 02:50:31 +0500

    Created a Word Counter Project

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Declare a constant 'answer' and assign it to the result of an inquirer.prompt function
+
+const answer: {
+  Sentence: string;
+} = await inquirer.prompt({
+  name: "Sentence",
+  type: "input",
+  message: "Please enter a sentence to count the words",
+});
+const words = answer.Sentence.trim().split(" ");
+
+//print the array of words to the console
+
+console.log(words);
+
+//print the word count of the sentence to the console
+console.log(`The number of words in this sentence is ${words.length}`);
